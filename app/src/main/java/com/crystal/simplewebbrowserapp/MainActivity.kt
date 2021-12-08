9ca632414a06824023176c96040a39e91e3996d8
kwonsoojeong <0401tnwjd@gmail.com>
2021-12-08 16:14:09 +0900

    Layout 작업 - WebView, addressEditTextView init setting

--- app/src/main/java/com/crystal/simplewebbrowserapp/MainActivity.kt
+++ app/src/main/java/com/crystal/simplewebbrowserapp/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.crystal.simplewebbrowserapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        bindViews()
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+            return@setOnEditorActionListener false //이이벤트를 끝낼건지 다른곳에서 핸들링하게 둘건지
+        }
+
+    }
+
 }
